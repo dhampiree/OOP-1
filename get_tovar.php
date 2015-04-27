21028a5933e0bf29b02c8909ce517a0ef507a1bc
Karponter <karponter@bigmir.net>
2015-04-27 11:19:14 +0300

    Update get_tovar.php

--- get_tovar.php
+++ get_tovar.php
@@ -1,3 +1,34 @@
 <? php
-
+	function get_tovars($cat_id) {
+		
+		if (!is_integer($cat_id) and $cat_id != 0) {
+			console.log('ERROR: get_tovar: category id is not integer');
+			exit();
+		}
+		
+		'
+		SELECT G_CAT_connection.goods_id
+		FROM G_CAT_connection
+		WHERE `categ_id` ='.$cat_id.'
+		'
+		
+		'
+		SELECT *
+		FROM `Goods_exemplars`
+		WHERE `goods_id` IN 
+		'
+	}
 ?>
+
+SELECT *
+FROM Goods_exemplars
+WHERE Goods_exemplars.goods_id
+IN ( 
+    SELECT G_CAT_connection.goods_id
+    FROM G_CAT_connection
+    WHERE G_CAT_connection.categ_id =1
+)
+LEFT JOIN Prices
+    ON Prices.exemplar_id = Goods_exemplars.id
+LEFT JOIN Storage
+    ON Storage.exemplar_id = Goods_exemplars.id
