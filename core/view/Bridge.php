d1eef13b378c5b18b4f40643e1d1d80369a93932
Vlad Dotsenko <karponter@bigmir.net>
2015-05-07 12:02:22 +0300

    Create Bridge.php

---
+++ core/view/Bridge.php
@@ -0,0 +1,18 @@
+<?php
+	namespace view;
+
+class Bridge {
+	private $JSON_handler;
+
+	function __construct() {
+		$this->JSON_handler = new JSON_handler();
+	}
+
+	function getTovars($category) {
+		$list = new TovarList($category);
+		return $this->JSON_handler->pack(
+			$list->getTovars()
+		);
+	}
+}
+?>
