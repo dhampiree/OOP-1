f5d35a18ebe982ef6f38f44bdac20cc9e65eca4c
Vlad Dotsenko <karponter@bigmir.net>
2015-05-07 11:40:40 +0300

    Create logic.php

---
+++ core/logic.php
@@ -0,0 +1,28 @@
+<?php
+	namespace logic;
+	
+class JSON_handler {
+	function pack($data) {
+		return json_encode($data);
+	}
+
+	function unpack($data) {
+		return json_decode($data);
+	}
+}
+
+class Bridge {
+	private $JSON_handler;
+
+	function __construct() {
+		$this->JSON_handler = new JSON_handler();
+	}
+
+	function getTovars($category) {
+		$list = new TovarList($category);
+		return $this->JSON_handler->pack(
+			$list->getTovars()
+		);
+	}
+}
+?>
