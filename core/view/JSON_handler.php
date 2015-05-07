4ea77ad6225f7fb28e97a1d252d7390d65f0f7a5
Vlad Dotsenko <karponter@bigmir.net>
2015-05-07 12:02:36 +0300

    Update JSON_handler.php

--- core/view/JSON_handler.php
+++ core/view/JSON_handler.php
@@ -10,19 +10,4 @@ class JSON_handler {
 		return json_decode($data);
 	}
 }
-
-class Bridge {
-	private $JSON_handler;
-
-	function __construct() {
-		$this->JSON_handler = new JSON_handler();
-	}
-
-	function getTovars($category) {
-		$list = new TovarList($category);
-		return $this->JSON_handler->pack(
-			$list->getTovars()
-		);
-	}
-}
 ?>
