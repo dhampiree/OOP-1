bfd36e163ac73ffda4b01df59cd86639dfa4d375
Vlad Dotsenko <karponter@bigmir.net>
2015-05-07 11:37:45 +0300

    Create config.php

---
+++ core/config.php
@@ -0,0 +1,7 @@
+<?php
+	namespace config;
+	define('DB_HOST', 'mysql.hostinger.com.ua');
+	define('DB_USER', 'u372374362_user');
+	define('DB_PASS', '28890929');
+	define('DB_NAME', 'u372374362_db');
+?>
