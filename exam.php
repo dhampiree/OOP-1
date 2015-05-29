78b4624dd05e214fe21943d70270e0e059b706cf
Vlad Dotsenko <karponter@bigmir.net>
2015-05-29 10:14:17 +0300

    Update exam.php

--- exam.php
+++ exam.php
@@ -1,8 +1,8 @@
 <?php
 	define('DB_HOST', 'mysql.hostinger.com.ua');
-	define('DB_USER', 'u372374362_user');
-	define('DB_PASS', '28890929');
-	define('DB_NAME', 'u372374362_db');
+	define('DB_USER', 'u661215920_root');
+	define('DB_PASS', '1qa2w3');
+	define('DB_NAME', 'u661215920_bd');
 
 	Header("Content-Type: text/html; charset=utf-8");
 
@@ -20,29 +20,28 @@
 		die('Connect Error ('.$connection->connect_errno.') '.$connection->connect_error);
 	}
 
-	if (isset($_GET['type']) {
+	if (isset($_GET['type'])) {
 		$typetitle = $_GET['type'];
 	} else {
 		die('no type defined...');
 	}
 
 	$query = '
-		SELECT car_values.charact_value, characteristics.title
+		SELECT Avto.model_title, characteristics.titile, car_values.charact_value
 		FROM car_values
 
 		JOIN characteristics
 		ON characteristics.id_charact = car_values.charact_id
 
-		JOIN avto
-		ON avto.avto_id = car_values.avto_id
+		JOIN Avto
+		ON Avto.avto_id = car_values.avto_id
 
-		WHERE avto.type_id IN (
+		WHERE Avto.type_id IN (
 			SELECT type.id_type
 			FROM type
-			WHERE type.title = '.$typetitle.' 
+			WHERE type.title = "'.$typetitle.'" 
 		)
 	';
-
 	$result = simple_select($query, $connection);
 	var_dump($result);
 ?>
