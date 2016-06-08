645f1048d4d7a66e578afa3bec221766de779a15
Vlad Dotsenko <karponter@bigmir.net>
2016-06-08 16:13:53 +0300

    Create exam_KT.cpp

---
+++ exam_KT.cpp
@@ -0,0 +1,66 @@
+
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+class Vigenier
+{
+
+private:
+  string SECRET;
+  int SECRET_CAPACITY;
+
+  int shift (int idx) {
+    char c = this->SECRET[idx % this->SECRET_CAPACITY];
+    int mod = (isupper(c)) ? 'A' : 'a';
+    return c - mod;
+  }
+
+
+public:
+
+  Vigenier(string secret)
+  {
+    this->SECRET = secret;
+    this->SECRET_CAPACITY = secret.size();
+  }
+
+  void encode(string message) {
+
+    int INPUT_LIMIT = message.size();
+    char convertor = ' ';
+    for (int i = 0; i < INPUT_LIMIT; i++) 
+    {
+      if (!isalpha(message[i])) 
+      {
+        cout << message[i];
+        continue;
+      }
+      char base = (isupper(message[i])) ? 'A' : 'a';
+      convertor = base + ( message[i] + this->shift(i) - base) % 26;
+      cout << convertor;
+    }
+  }
+
+  ~Vigenier(){}
+  
+};
+
+int main(int argc, string argv[]) {
+
+  string  secret = "";
+  string  message = "";
+
+  cout << "Enter secret key: ";
+  cin >> secret;
+
+  cout << "Enter private message: ";
+  cin >> message;
+  
+  Vigenier vig(secret);
+  vig.encode(message);
+  
+  return 0;
+
+}
